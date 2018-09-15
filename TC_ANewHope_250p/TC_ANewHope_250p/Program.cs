21b04f2ecef3319b11714b0912451f2378da5d38
Rajiv Iyer <RAIx86@users.noreply.github.com>
2018-09-15 11:39:19 +0530

    Updated Initial Comments (modified status)
    Updated Initial Comments (modified status)

--- TC_ANewHope_250p/TC_ANewHope_250p/Program.cs
+++ TC_ANewHope_250p/TC_ANewHope_250p/Program.cs
@@ -4,7 +4,7 @@
 // Return Type : int
 // Arg Types : (int[], int[], int)
 
-// Status on TopCoder: Compiled Successfully; Cleared Initial/Sample Tests; Didn't clear Wider System Tests
+// Status on TopCoder: Compiled Successfully; Cleared Initial/Sample Tests
 // TopCoder Link(see link for Problem Statement): https://arena.topcoder.com/#/u/practiceCode/16656/45873/13585/1/327836
 
 using System;
