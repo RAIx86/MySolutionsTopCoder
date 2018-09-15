6ddbb7c18ecf0bb6f3dfeca296e1675dc845d58d
Rajiv Iyer <RAIx86@users.noreply.github.com>
2018-09-15 11:42:38 +0530

    Added Initial Comments
    Added Initial Comments

--- TC_ATaleOfThreeCities_500pts/TC_ATaleOfThreeCities_500pts/Program_TCSubMod.cs
+++ TC_ATaleOfThreeCities_500pts/TC_ATaleOfThreeCities_500pts/Program_TCSubMod.cs
@@ -1,4 +1,13 @@
-﻿using System;
+// TopCoder - Problem Name: ATaleOfThreeCities(500 points) [MEDIUM]
+// Class Name : ATaleOfThreeCities
+// Method Name : connect
+// Return Type : double
+// Arg Types : (int[], int[], int[], int[], int[], int[])
+
+// Status on TopCoder: Compiled Successfully; Cleared Initial/Sample Tests; Cleared Wider System Tests
+// TopCoder Link(see link for Problem Statement): https://arena.topcoder.com/#/u/practiceCode/1499/3281/3543/2/1499
+
+using System;
 
 //namespace TC_ATaleOfThreeCities_500pts
 //{
